cdc7ca946f4ec0849a1a320bf0c01629174c0b97
47363991@qq.com <47363991@qq.com>
2019-09-26 18:58:05 +0800

    up

---
+++ static/js/AliYch.js
@@ -0,0 +1,11 @@
+/**
+ * 阿里御城河回传代码（https://eco.alibaba.com）
+ * 2019-09-26
+ * YSPN
+ */
+
+const appKey = '68756856' // 御城河专用AppKey
+const appSecret = 'wsf6VJLJbIPWo0EwAqSd' // 御城河专用
+const topAppKey = '23396371'
+const appName = '淘易易订单'
+
